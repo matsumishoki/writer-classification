6f97e5329b02da4e08c94e9d5f941380fe92748f
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-02-04 15:40:31 +0900

    切り出し画像を保存するファイルを追加する

---
+++ make_cutting_image.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 21:09:46 2016
+
+@author: matsumi
+"""
+import numpy as np
+import scipy as sp
+from xml.etree import ElementTree
+import os
+import skimage
+from skimage.color import rgb2gray
+from skimage.filters import threshold_otsu
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import skimage.io
+
+# 検索するデータセットのファイルのtop_pathを指定する
+top_path = os.path.join("CVL_ConvNet_data")
+temp_list = [data_filepath for data_filepath in os.walk(top_path)]
+num_temp_list = len(temp_list)
+
+tup = temp_list[0]
+(dirpath, dirnames, filenames) = tup
+
+num_tup = len(tup)
+num_dirpath = len(dirpath)
+num_dir = len(dirnames)
+num_file = len(filenames)
+
+change_path = os.path.join("CVL_ConvNet_data")
+
+# image = plt.imread(os.path.join(change_path, "0001-1-cropped.png"))
+
+
+# 画像サイズを指定する
+image_size = 200
+cut_image_edge_size = 200
+y_points_history = []
+# file数分のループを回す
+for filename in filenames:
+    loadFileFullpath = os.path.join(dirpath, filename)
+    image = plt.imread(os.path.join(dirpath, filename))
+
+    # 画像のy(縦座標)座標の総数を取得する
+    image_y = image.shape[0]
+
+    # 画像のx座標(横座標)の総数を取得する
+    image_x = image.shape[1]
+
+    # xとy共に200ずつの間隔で座標を取得する
+    y_start_point = range(0, image_y, image_size)
+    x_start_point = range(0, image_x, image_size)
+    num_y_start_point = len(y_start_point)
+    num_x_start_point = len(x_start_point)
+    # print "num_y_start_point:", num_y_start_point
+    # print "num_x_start_point:", num_x_start_point
+
+    # 訓練データを生成する
+    # 横座標を固定で縦座標を横にズラしていき，200*200の画像を取得する
+    # 端の余った画像部分は使用しない
+    for x in range(num_x_start_point-1):
+        x_point = x_start_point[x]
+        for y in range(num_y_start_point-1):
+            y_point = y_start_point[y]
+#            print "train_x_point:", x_point
+#            print "train_y_point:", y_point
+            # 画像データの名前と拡張子を分離する
+            name, ext = os.path.splitext(filename)
+            train_cropped_image = image[y_point:y_point+image_size,
+                                        x_point:x_point+image_size]
+            plt.imshow(train_cropped_image, cmap=plt.cm.gray)
+            save_path = "C:\\Users\\matsumi\\Desktop\\writer classification\\CVL_ConvNet_pow\\"
+            name = name + "_" + str(y)
+            saveFilename = save_path + name + ".png"   # name + 新しい拡張子(.png)
+            print saveFilename
+            skimage.io.imsave(saveFilename, train_cropped_image)
+            plt.show()
+            plt.draw()
+
+    # テストデータを生成する
+    # 横座標をランダムに選び，選んだ位置から縦に200横に200の画像サイズを指定し，画像を切り取る
+    # 切り取る時に縦200，横200の画像サイズが切り取れる位置を指定できるようにする
+    x_select_points = image_x - cut_image_edge_size
+    y_select_points = image_y - cut_image_edge_size
+    x_select_point = np.random.permutation(x_select_points)
+    y_select_point = np.random.permutation(y_select_points)
+
+    test_cropped_image = image[y_select_point[0]:y_select_point[0]+image_size,
+                               x_select_point[0]:x_select_point[0]+image_size]
+#    print "test_x_point:", x_select_point[0]
+#    print "test_y_point:", y_select_point[0]
+    y_points_history.append(y_select_point[0])
+print len(y_points_history)
+#    plt.imshow(test_cropped_image, cmap=plt.cm.gray)
+
